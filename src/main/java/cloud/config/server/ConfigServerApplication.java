1989ee601b0e26183bad862b1bcdd9a9cbcefccb
yuhaitao <linniku123@163.com>
2018-04-25 07:09:41 +0800

    ääupdate

--- src/main/java/cloud/config/server/ConfigServerApplication.java
+++ src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("xxxx4");
 	}
 }
